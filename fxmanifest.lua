fb7305e9e8cfa6139dfeefdd3d21cc8c4a8821b6
zita656 <75706294+zita656@users.noreply.github.com>
2022-04-22 17:39:06 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "oxyrun"
+description "OxyRun"
+version "1.2"
+
+client_scripts {
+	'client/main.lua',
+	'config.lua'
+}
+
+server_scripts {
+	'server/main.lua',
+	'config.lua'
+}
